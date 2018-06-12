f0fb6226d0b7027d0ef0f8bcfef3320f9f3e1239
Your Name <alcantar.d.isaac.e@gmail.com>
2018-06-12 09:06:06 -0500

    profe

---
+++ Hola.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+/*Hola :V*/
+using namespace std;
+
+main()
+{
+	cout<<"Hola mundo"<<endl;
+	
+	cout<<"hola profeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee"<<endl;
+}
